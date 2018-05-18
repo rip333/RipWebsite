15d528d46cb94fe5fc2674f4c341d03d986e8fd1
rip333 <ripbritton3@gmail.com>
2018-05-18 11:50:46 -0500

    Video game page

---
+++ RipWebsite/RipBrittonWebsite/Controllers/VideoGamesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RipBrittonWebsite.Controllers
+{
+    public class VideoGamesController : Controller
+    {
+        // GET: VideoGames
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
