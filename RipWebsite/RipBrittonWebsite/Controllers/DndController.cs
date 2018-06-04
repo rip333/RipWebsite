c85ec04b25a34874d9fd945c25087b5b70503518
rip333 <ripbritton3@gmail.com>
2018-06-03 21:14:51 -0500

    dnd index

---
+++ RipWebsite/RipBrittonWebsite/Controllers/DndController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using PokefamCore.Models;
+using PokefamCore.Utility;
+
+namespace RipBrittonWebsite.Controllers
+{
+    public class DndController : Controller
+    {
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetModifier(TypeOf attackType, TypeOf defenderPrimaryType,
+            TypeOf defenderSecondaryType = TypeOf.Pure)
+        {
+            TypeChart typeChart = new TypeChart();
+            var modifier = typeChart.GetModifier(attackType, defenderPrimaryType, defenderSecondaryType);
+            return View(modifier);
+        }
+    }
+}
